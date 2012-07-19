31d0e8261a8de776990935a38a9d08a630801260
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2012-07-19 22:58:43 +0200

    Initial Commit

---
+++ Project Euler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project Euler
+//
+//  Created by Christoffer Buchholz on 19/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
