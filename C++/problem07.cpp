78e17fdb876dd70266e896476e2d32fe486450f9
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2012-09-04 08:07:56 +0200

    add description to all problems

--- C++/problem07.cpp
+++ C++/problem07.cpp
@@ -1,3 +1,10 @@
+/**
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+ * that the 6th prime is 13.
+ *
+ * What is the 10 001st prime number?
+ **/
+
 #include <iostream>
 #include <cmath>
 
@@ -22,8 +29,8 @@ bool is_prime(unsigned long long n)
 
 int main(int argc, const char * argv[])
 {
-    int count;
-    int prime;
+    int count = 0;
+    int prime = 0;
 
     for (int i = 0; ; i++) {
         if (is_prime(i))
