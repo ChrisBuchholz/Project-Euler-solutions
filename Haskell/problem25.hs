d8e6e42b8fdf0fcff442fe5017841b793b26947e
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2012-12-06 09:08:11 +0100

    added solution for problem 35

--- Haskell/problem25.hs
+++ Haskell/problem25.hs
@@ -1,50 +1,5 @@
-{-
- - The Fibonacci sequence is defined by the recurrence relation:
- -
- - Fn = Fn1 + Fn2, where F1 = 1 and F2 = 1.
- - Hence the first 12 terms will be:
- -
- - F1 = 1
- - F2 = 1
- - F3 = 2
- - F4 = 3
- - F5 = 5
- - F6 = 8
- - F7 = 13
- - F8 = 21
- - F9 = 34
- - F10 = 55
- - F11 = 89
- - F12 = 144
- -
- - The 12th term, F12, is the first term to contain three digits.
- -
- - What is the first term in the Fibonacci sequence to contain 1000 digits?
- -}
-
 fibonacci :: [Integer]
 fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
 
 main :: IO ()
 main = print $ head [i | i <- [1..], (==1000) . length . show $ fibonacci !! i]
-
-
-{-
- - f = 0 : 1 : zipWith (+) f (tail f)
- -
- - take 1 f
- - take 2 f
- - f = 0 : 1 : <thunk>
- - 
- - take 3 f
- - f = 0 : 1 : 1 : <thunk>
- -
- - take 4 f
- - f = 0 : 1 : 1 : 2 : <thunk> 
- -
- - take 5 f
- - f = 0 : 1 : 1 : 2 : 3 : <thunk>
- -
- - take 6 f
- - f = 0 : 1 : 1 : 2 : 3 : 5 : <thunk> 
- -}
---
+++ Haskell/problem35.hs
@@ -0,0 +1,20 @@
+import Data.Char
+
+rotate :: [a] -> [a]
+rotate (x:xs) = xs ++ [x]
+
+rotations :: [a] -> [[a]]
+rotations x = take (length x) $ iterate rotate x
+
+prime :: Integral a => a -> Bool
+prime n
+	| n < 2														                  = False
+	| n == 2													                  = True
+	| length [x | x <- [2..(floor . sqrt $ fromIntegral n)], n `mod` x == 0] == 0 = True 
+	| otherwise                                                                   = False
+
+circularPrime :: Show a => a -> Bool
+circularPrime n = and $ map prime $ map (\ns -> foldl (\x y -> 10 * x + y) 0 ns) $ rotations $ map digitToInt $ show n
+
+main :: IO ()
+main = print $ (length [n | n <- [1,3..10^6], notElem 5 $ map digitToInt $ show n, circularPrime n]) + 2
